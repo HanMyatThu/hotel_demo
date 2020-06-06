6efb4cb02a9394291977f6064568d3ade7dceba7
Han Myat Thu <hanmyatthu111@gmail.com>
2020-06-06 19:55:25 +0630

    add jwt middleware

--- app/User.php
+++ app/User.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use App\Model\Booking;
+use App\Model\BookingRequest;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -36,4 +39,42 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relationship
+     */
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function bookingRequests()
+    {
+        return $this->hasMany(BookingRequest::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,15 @@ use Illuminate\Http\Request;
 Route::apiResource('/hotels', 'HotelController');
 
 Route::apiResource('/hotels/{hotel}/rooms', 'RoomController');
+
+
+Route::group([
+    'prefix' => 'auth'
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('register','AuthController@register');
+});
